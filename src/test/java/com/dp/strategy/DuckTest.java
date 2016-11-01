fd2391d6515ba18be2b79a738f0933570b272962
Neo <arun2pratap@gmail.com>
2016-11-01 20:57:15 +0530

    test class added.

--- src/test/java/com/dp/strategy/DuckTest.java
+++ src/test/java/com/dp/strategy/DuckTest.java
@@ -1,7 +1,18 @@
 package com.dp.strategy;
 
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.Enclosed;
+
 /**
  * Created by Neo on 10/19/2016.
  */
 public class DuckTest {
+
+    @Test
+    public void testFly(){
+        FlyBehaviour fly;
+        Assert.assertEquals("","");
+    }
 }
--- src/test/java/com/dp/strategy/FlyBehaviour.java
+++ src/test/java/com/dp/strategy/FlyBehaviour.java
@@ -4,4 +4,5 @@ package com.dp.strategy;
  * Created by Neo on 10/20/2016.
  */
 public interface FlyBehaviour {
+    public void fly();
 }
